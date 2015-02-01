f7d881cbddf017d4b34b9b1307d36bc3ffcbd1fa
mitchellnau <mmh327@nau.edu>
2015-02-01 14:05:53 -0700

    page framework

--- Theatre_CS399/urls.py
+++ Theatre_CS399/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'Theatre_CS399.views.home', name='home'),
+    url(r'^$', 'Theatre_CS399.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
