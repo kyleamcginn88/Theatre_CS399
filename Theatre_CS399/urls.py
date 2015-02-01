b4e2373d57a55aacc76be86ad0bab2a9358fed5b
kyleamcginn88 <kyle.a.mcginn@gmail.com>
2015-02-01 14:24:05 -0700

    Added 4 main pages

--- Theatre_CS399/urls.py
+++ Theatre_CS399/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$', 'Theatre_CS399.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+	url(r'^$', 'Theatre_CS399.views.movies', name='movies'),
+	url(r'^$', 'Theatre_CS399.views.performances', name='performances'),
+	url(r'^$', 'Theatre_CS399.views.store', name='store'),
+	url(r'^$', 'Theatre_CS399.views.location', name='location'),
+	
     url(r'^admin/', include(admin.site.urls)),
 )
--- Theatre_CS399/views.py
+++ Theatre_CS399/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html', {})
+	
+def store(request):
+    return render(request, 'store.html', {})
+	
+def performances(request):
+    return render(request, 'performances.html', {})
+	
+def location(request):
+    return render(request, 'location.html', {})
+	
+def movies(request):
+    return render(request, 'movies.html', {})
