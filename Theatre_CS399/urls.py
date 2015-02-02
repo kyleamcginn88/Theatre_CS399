ddf28d1be6832409ca6c6e8775398a88a9b56127
mitchellnau <mmh327@nau.edu>
2015-02-01 17:12:11 -0700

    fixed regex errors with urls

--- Theatre_CS399/urls.py
+++ Theatre_CS399/urls.py
@@ -3,10 +3,10 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^$', 'Theatre_CS399.views.home', name='home'),
-	url(r'^$', 'Theatre_CS399.views.movies', name='movies'),
-	url(r'^$', 'Theatre_CS399.views.performances', name='performances'),
-	url(r'^$', 'Theatre_CS399.views.store', name='store'),
-	url(r'^$', 'Theatre_CS399.views.location', name='location'),
+	url(r'^movies$', 'Theatre_CS399.views.movies', name='movies'),
+	url(r'^performances$', 'Theatre_CS399.views.performances', name='performances'),
+	url(r'^store$', 'Theatre_CS399.views.store', name='store'),
+	url(r'^location$', 'Theatre_CS399.views.location', name='location'),
 	
     url(r'^admin/', include(admin.site.urls)),
 )
