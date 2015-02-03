183657de6dee05cf82798d01b0ce75367bde3b7b
bsaganey <bs366@nau.edu>
2015-02-02 23:30:34 -0700

    Added performances table
    Added Bands dates of performances

--- Theatre_CS399/views.py
+++ Theatre_CS399/views.py
@@ -7,7 +7,20 @@ def store(request):
     return render(request, 'store.html', {})
 	
 def performances(request):
-    return render(request, 'performances.html', {})
+	list_performances = [('1/15/15', 'Fish Pump', 'Techno' ),
+	('2/25/15', '7pac' ,'Hip-Hop'),
+	('3/1/15', 'J-ci and Jolow','R&B' ),
+	('4/18/15', 'King of Lions', 'Alternative Rock'),
+	('5/5/15', 'Dr Jays', 'Rap' ),
+	('6/7/15', 'Nerdvana' , 'Grunge'),
+	('7/11/15', 'My Chemical Love' , 'Punk'),
+	('8/21/15', 'T.I. Guy' , 'Hip-Hop'),
+	('9/13/15', 'RUN D & C' ,'Hip-Hop'),
+	('10/16/15', 'Food Thrower', 'Alternative Rock' ),
+	('11/23/15', 'Dark Keys' ,'Alternative Rock'),
+	('12/11/15', 'IceBox','Rap' ),
+	('12/31/15', 'boyz II Gentlemen', 'R&B' )]
+	return render(request, 'performances.html', {'list_performances': list_performances})
 	
 def location(request):
     return render(request, 'location.html', {})
@@ -31,3 +44,4 @@ def movies(request):
 	('Inferno of Action' , 'Fiery flaming spiral of death.') ,
 	('Soldier of Justice' , 'Judge Dredd is back!') ]
     return render(request, 'movies.html', {'list_movies': list_movies})
+    
